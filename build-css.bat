28abd09a0a92e05314c23842e2a6b7899e74546a
Josh <dev@cldv.org>
2022-08-20 00:00:53 +0200

    add comment to generated css

--- build-css.bat
+++ build-css.bat
@@ -1,1 +1,14 @@
 python -m scss < src/styles.scss -o dist/vdlcss-min.css -I src/
+
+cd dist/
+move vdlcss-min.css vdlcss-min.css.tmp
+
+echo /* >> vdlcss-min.css
+echo  * vdlcss >> vdlcss-min.css
+echo  * License: MIT>> vdlcss-min.css
+echo  * Source: http://www.cldv.org/vdlcss/ >> vdlcss-min.css
+echo  * by ColdIV >> vdlcss-min.css
+echo  */ >> vdlcss-min.css
+
+type vdlcss-min.css.tmp >> vdlcss-min.css
+del vdlcss-min.css.tmp
